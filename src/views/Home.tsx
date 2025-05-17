b161c662df867e6908ba63754ceff93b5f1d5bdf
cjysmb <cjsamboa@gmail.com>
2025-05-18 02:48:52 +0800

    FIX!: Update speed insights with react

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,7 +2,7 @@ import { AboutUs } from "../components/about_us/aboutUs";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 import { OurProgram } from "../components/our_programs/ourProgram";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export const Home = () => {
     return (
