244d88e2e634ad0dc0cf26450e7c53d68d12fd82
cjysmb <cjsamboa@gmail.com>
2025-05-18 02:46:37 +0800

    FEAT!: Add speed insights

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,11 +2,13 @@ import { AboutUs } from "../components/about_us/aboutUs";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 import { OurProgram } from "../components/our_programs/ourProgram";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const Home = () => {
     return (
         <div className="w-full snap-y snap-mandatory">
+
+            <SpeedInsights />
             <Header />
             <OurProgram />
             <AboutUs />
