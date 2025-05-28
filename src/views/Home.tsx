24068bf16aa73e6d98b00a6dbda8ed6c0a291c45
cjysmb <cjsamboa@gmail.com>
2025-05-28 23:42:52 +0800

    FIX: Update mobile background and HR

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,13 +4,14 @@ import { Header } from "../components/header";
 import { OurProgram } from "../components/our_programs/ourProgram";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import Horizontal from "../assets/icons/about/hr.svg";
+import mHorizontal from "../assets/icons/about/mobileHr.svg";
 import { ContactUs } from "../components/contactUs";
 
-export const HorizontalLine = () => {
+export const  HorizontalLine = () => {
     return (
-        <div className="absolute left-1/2 top-0 -translate-x-1/2 transform z-0 hidden
-        md:block">
-            <img src={Horizontal} alt="vector" />
+        <div className="w-full flex justify-center items-center mb-[18px] md:mb-[60px]">
+            <img src={Horizontal} alt="vector" className="w-[80%] h-full hidden md:block"/>
+            <img src={mHorizontal} alt="vector" className="block md:hidden"/>
         </div>
     )
 }
@@ -21,7 +22,7 @@ export const Home = () => {
             <SpeedInsights />
             <Header />
             {/* WIP: mobile backround */}
-            <div className={`bg-[url('/mobileBackground.svg')] sm:bg-[url('/background.svg')] bg-cover z-1 relative`}>
+            <div className={`bg-[url('/mobileBackground.png')] md:bg-[url('/background.svg')]`}>
                 <OurProgram />
                 <AboutUs />
                 <ContactUs />
