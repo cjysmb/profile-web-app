31673ca346be7baf3dd242c8e4a782569e82a6b8
cjysmb <cjsamboa@gmail.com>
2026-03-17 22:58:51 +0800

    TASK: Add simple transition to show the faq content

--- src/components/about_us/faq/AccordionItem.tsx
+++ src/components/about_us/faq/AccordionItem.tsx
@@ -26,16 +26,16 @@ const AccordionItemComponent: FC<AccordionItemProps> = ({ title, children, isAct
         {/* <ChevronDown
           className={`w-5 h-5 transition-transform duration-200 ${isOpen ? 'rotate-180' : ''}`}
         /> */}
-        <img src={Caret} alt="caret-icon" className={`${isActive && 'rotate-180'}`} />
+        <img src={Caret} alt="caret-icon" className={`transition-transform duration-300 ${isActive && 'rotate-180'}`} />
       </button>
-      {isActive && (
-        <div
-          id={`accordion-content-${title}`}
-          className="px-4 py-2 text-gray-700"
-        >
+      <div
+        id={`accordion-content-${title}`}
+        className={`overflow-hidden transition-all duration-300 ${isActive ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
+      >
+        <div className="px-4 py-2 text-gray-700">
           {children}
         </div>
-      )}
+      </div>
     </div>
   );
 };
